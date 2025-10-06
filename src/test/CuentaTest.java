cf91a04bb472d3c2f2a9fe096b67e48c8332e5cd
Sergions1 <sergionunezsierra1@gmail.com>
2025-10-06 17:56:23 +0200

    1º Test0014 erroneo

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -12,11 +12,13 @@ import pkg.Cuenta;
 
 class CuentaTest {
 
-	private static Cuenta cuenta;
+	private static Cuenta cuenta, cuenta12345, cuenta67890;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		cuenta = new Cuenta(0);
+		cuenta12345 = new Cuenta(50);
+		cuenta67890 = new Cuenta(0);
 	}
 
 	@AfterAll
@@ -42,6 +44,32 @@ class CuentaTest {
 	void testRetirar() {
 		cuenta.retirar(500);
 		assertEquals(-500, cuenta.getSaldo());
+	}
+	
+	@Test
+	void test0014() {
+		assertEquals(50,cuenta12345.getSaldo());
+		assertEquals(0,cuenta67890.getSaldo());
+		
+		cuenta12345.retirar(200);
+		assertEquals(-150,cuenta12345.getSaldo());
+		cuenta67890.retirar(350);
+		assertEquals(-350,cuenta67890.getSaldo());
+		cuenta12345.ingresar(100);
+		assertEquals(-50,cuenta12345.getSaldo());
+		cuenta67890.retirar(200);
+		assertEquals(-350,cuenta67890.getSaldo());
+		cuenta67890.retirar(150);
+		assertEquals(-500,cuenta67890.getSaldo());
+		cuenta12345.retirar(200);
+		assertEquals(-250,cuenta12345.getSaldo());
+		cuenta67890.ingresar(50);
+		assertEquals(-450,cuenta67890.getSaldo());
+		cuenta67890.retirar(100);
+		assertEquals(-450,cuenta67890.getSaldo());
+
+		
+
 	}
 
 }
