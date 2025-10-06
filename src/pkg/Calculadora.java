420bc74d9eec0bb9e6ffb4814aa6658702d3ebcb
Sergions1 <sergionunezsierra1@gmail.com>
2025-10-06 17:14:35 +0200

    Clase creada

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,24 @@
+package pkg;
+
+public class Calculadora {
+
+	public static Integer suma(int i, int j) {
+		return i+j;
+	}
+
+	public static Integer resta(int i, int j) {
+		return i-j;
+	}
+
+	public static Integer multiplica(int i, int j) {
+		return i*j;
+	}
+
+	public static float divide(int i, int j) {
+		if(j==0)
+			return -1;
+		else
+			return i/j;
+	}
+
+}
--- src/test/CalculadoraTest.java
+++ src/test/CalculadoraTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pkg.Calculadora;
+
 class CalculadoraTest {
 
 	@BeforeAll
